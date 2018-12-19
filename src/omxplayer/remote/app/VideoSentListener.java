8b990f51a8e5e89c5533bfc74f457fb3be8fa7b6
Mohamed Osama <m.osama@fetchr.us>
2018-12-19 21:12:18 +0400

    remove missing files

--- src/omxplayer/remote/app/VideoSentListener.java
+++
@@ -1,9 +0,0 @@
-package omxplayer.remote.app;
-
-import omxplayer.remote.app.utils.Utils.SendStatus;
-
-public interface VideoSentListener {
-
-	public void finishedSending(SendStatus state, String fileName);
-	
-}
--- src/omxplayer/remote/app/network/CommandSender.java
+++
@@ -1,7 +0,0 @@
-package omxplayer.remote.app.network;
-
-public interface CommandSender {
-
-	public String send(String cmd, String... optionalParams);
-	
-}
