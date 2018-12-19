d0f440c7431f630359d33ca6c8dcd27f4f7afe5d
Mohamed Osama <m.osama@fetchr.us>
2018-12-19 23:21:13 +0400

    use java 7 syntax

--- app/src/main/java/omxplayer/remote/app/dialogs/AdminPasswordDialog.java
+++ app/src/main/java/omxplayer/remote/app/dialogs/AdminPasswordDialog.java
@@ -17,63 +17,68 @@ import omxplayer.remote.app.utils.Utils;
 
 public class AdminPasswordDialog extends Dialog {
 
-	private Context context;
-	private CommandSender commandSender;
-	private EditText passwordEditText;
+    private Context context;
+    private CommandSender commandSender;
+    private EditText passwordEditText;
 
-	public AdminPasswordDialog(Context context, CommandSender commandSender) {
-		super(context);
-		this.context = context;
-		this.commandSender = commandSender;
-		setupDialog();
-	}
+    public AdminPasswordDialog(Context context, CommandSender commandSender) {
+        super(context);
+        this.context = context;
+        this.commandSender = commandSender;
+        setupDialog();
+    }
 
-	private void setupDialog() {
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.password_screen);
-		final Window window = getWindow();
-		window.setLayout(WindowManager.LayoutParams.WRAP_CONTENT,
-				WindowManager.LayoutParams.WRAP_CONTENT);
-		window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
-		window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    private void setupDialog() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.password_screen);
+        final Window window = getWindow();
+        window.setLayout(WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
-		passwordEditText = (EditText) findViewById(R.id.passId);
+        passwordEditText = (EditText) findViewById(R.id.passId);
 
-		passwordEditText.addTextChangedListener(new TextWatcher() {
+        passwordEditText.addTextChangedListener(new TextWatcher() {
 
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before,
-					int count) {
-				if (s.toString().equals(Utils.adminPassword)) {
-					// perform close function
-					dismiss();
-					if (!Utils.connected) {
-						Toast.makeText(context, "Not conected",
-								Toast.LENGTH_SHORT).show();
-						return;
-					}
-					new Thread(commandSender.send(Utils.SSHCommands.shutdownHostCmd)).start();
-				}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                if (s.toString().equals(Utils.adminPassword)) {
+                    // perform close function
+                    dismiss();
+                    if (!Utils.connected) {
+                        Toast.makeText(context, "Not conected",
+                                Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            commandSender.send(Utils.SSHCommands.shutdownHostCmd);
+                        }
+                    }).start();
+                }
 
-			}
+            }
 
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count,
-					int after) {
-			}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+            }
 
-			@Override
-			public void afterTextChanged(Editable s) {
-			}
-		});
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
 
-	}
+    }
 
-	@Override
-	public void show() {
-		if (passwordEditText != null) {
-			passwordEditText.setText("");
-		}
-		super.show();
-	}
+    @Override
+    public void show() {
+        if (passwordEditText != null) {
+            passwordEditText.setText("");
+        }
+        super.show();
+    }
 }
